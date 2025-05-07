38bf2a57d7ae927e11e2b7d717a682708b54ad75
Nick McKay <nickmckay@users.noreply.github.com>
2025-05-07 14:01:00 -0700

    nick updates

--- PALMOD2LiPD.R
+++ PALMOD2LiPD.R
@@ -1,16 +1,16 @@
 # function to read PalMOD format into LiPD
-devtools::install_github("nickmckay/lipdR")
+#devtools::install_github("nickmckay/lipdR")
 
 # this version does not split the metadata and data by retrieval
 # and adds the updated age model to the paleo data
 
-PALMOD2LiPD <- function(x, path = "dataLiPD"){   # x is list with data
+PALMOD2LiPD <- function(x, path = "dataLiPD/"){   # x is list with data
   require(lipdR)
   require(tidyverse)
 
   #will use this later.
   createTSid <- function(){
-    return(paste(c("R",sample(c(letters,LETTERS,seq(0,9)),size = 10,replace=TRUE)),collapse = ""))
+    return(paste(c("PM",sample(c(letters,LETTERS,seq(0,9)),size = 10,replace=TRUE)),collapse = ""))
   }
 
   # file
@@ -27,7 +27,7 @@ PALMOD2LiPD <- function(x, path = "dataLiPD"){   # x is list with data
   L$lipdVersion <- 1.3 # probably needs an update
 
   #NOTE THIS!!!!
-  L$archiveType <- "marine sediment"
+  L$archiveType <- "MarineSediment"
 
   #geo
   L$geo <- list()
@@ -66,25 +66,25 @@ PALMOD2LiPD <- function(x, path = "dataLiPD"){   # x is list with data
              "DataLink",
              "TSuID" # keeping this instead of randomly generated one
              )
-  
+
   # PALMOD sometimes has the following addtional fields
   # DataDOI: not relevant as we have DataLink
   # RetrievalNumber: not relevant, only for internal use
   # Publication: for publications without link or doi. These are currently ignored, but we could include them in the pub field, no?
-  # PublicationLink: for publications without doi, but with alternative identifier  
-  
+  # PublicationLink: for publications without doi, but with alternative identifier
+
   paleoLipd <- c("description", "variableName","variableType","units","analyticalError","reproducibility","measurementInstrument","measurementLaboratory","sampleThickness","measurementMaterial","sensorSpecies","measurementN","measurementSizeFraction","notes","habitatSeason","habitatDepth","equilibriumOffset","calibrationEquation","calibrationUncertainty","calibrationDOI","transferFunctionTrainingSet","transferFunctionUncertainty","transferFunctionDOI", 'hasPubDOI', 'hasDataLink', "TSid")
-  
+
   # check names
   #cbind.data.frame(paleo, paleoLipd)
 
   makeMeasurementTable <- function(meta, data){
     mt <- vector(mode = "list", length = nrow(meta))
     orig.data.url <- c()
-    # not all files have all meta data fields 
+    # not all files have all meta data fields
     Paleo <- paleo[paleo %in% names(meta)]
     PaleoLipd <- paleoLipd[paleo %in% names(meta)]
-    
+
     for(i in 1:nrow(meta)){
 
       for(p in 1:length(Paleo)){
@@ -105,19 +105,19 @@ PALMOD2LiPD <- function(x, path = "dataLiPD"){   # x is list with data
     }
     mt
   }
-  
+
   paleoData <- list(makeMeasurementTable(e$meta, e$data)) # does this need to be a list?
   names(paleoData) <- NULL
-  
+
   L$paleoData[[1]]$measurementTable <- paleoData
   if(exists("orig.data.url")){
     L$originalDataUrl <- orig.data.url
   }
-  
+
   # this adds a primary age identifier to the updated age model data
   L$paleoData[[1]]$measurementTable[[1]]$medianAge_kaBP$primaryAgeColumn <- TRUE
-  
-  
+
+
   #chronData ####
   chronMeta <- c("ChronSource", "ChronDOI")
   chronMetaLipd <- c("chronURL", "chronDOI")
@@ -149,68 +149,71 @@ PALMOD2LiPD <- function(x, path = "dataLiPD"){   # x is list with data
            "ChronCalendarAgeMax_kaBP",
            "ChronAgeRejected",
            "ChronNotes")
-  
+
   cdcLiPD <- c("dateType","depthTop","depthBottom","depth","thickness","age","ageUncertainty","measurementMaterial","sensorSpecies","measurementN","tuneTarget","tuneVariable","labID","age14C","age14CUncertainty","age14CHi","age14CLow","reservoirAge","reservoirAgeUncertainty", "calibrationCurve","age","ageUncertainty1sigLo", "ageUncertainty1sigHi", "ageUncertainty2sigLo", "ageUncertainty2sigHi",  "rejected","notes")
-  
+
   # check names
   #cbind.data.frame(cdc, cdcLiPD)
-  
+
   chronData <- e$chron[, names(e$chron) %in% cdc]
-  
+
   if(!is.null(chronData)){ # catch cases without chronology data
     nGoodCol <- sum(colSums(!is.na(chronData))>0)
     mt <- vector(mode = "list", length = nGoodCol)
     i <- 1
-    
+
     for(v in 1:length(cdc)){
       if(any(!is.na(chronData[[cdc[[v]]]]))){
         mt[[i]]$values <-  chronData[[cdc[[v]]]]
+        if(is.logical(mt[[i]]$values)){
+          mt[[i]]$values <- as.character(mt[[i]]$values)
+        }
         mt[[i]]$TSid <- createTSid()
         mt[[i]]$variableName <- cdcLiPD[[v]]
         mt[[i]]$number <- v
-        
+
         #metadata
         for(m in 1:length(chronMeta)){
           if(any(!is.na(e$chron[[chronMeta[[m]]]]))){
             mt[[i]][[chronMetaLipd[m]]] <- unique(e$chron[[chronMeta[m]]]) # note that link between dating point and meta is lost
           }
-          
+
         }
-        
+
         i <- i+1
-        
+
       }
     }
-    
+
     # add units
     depthChronUnits <- c("depthTop","depthBottom","depth","thickness")
     ageChronUnits <- c("age","ageUncertainty", "age14C","age14CUncertainty","age14CHi","age14CLow","reservoirAge","reservoirAgeUncertainty", "age","ageUncertainty1sigLo", "ageUncertainty1sigHi", "ageUncertainty2sigLo", "ageUncertainty2sigHi")
-    
-    mt <- lapply(mt, function(n){
+
+    mt2 <- purrr::map(mt, function(n){
       if(n$variableName %in% depthChronUnits){n$units <- 'cm'}
       else if(n$variableName %in% ageChronUnits){n$units <- 'kyr'}
-      n
+      return(n)
     })
-    
+
     # faster to do this earlier
     if(nGoodCol > 0){L$chronData[[1]]$measurementTable[[1]] <- mt}
   }
-  
+
   # no longer needed now included in data
   # create chron model
   # summary table
-  
+
   # stc <- c("depth_m","meanAge_kaBP","loAge_kaBP","medianAge_kaBP","hiAge_kaBP")
-  # 
+  #
   # stcLiPD <- c("depth","age","ageLow","ageMedian","ageHi")
   # stu <- c("m","ka","ka","ka","ka")
-  # 
+  #
   # sumData <- e$AgeModel[, names(e$AgeModel) %in% stc]
-  # 
+  #
   # nGoodCol <- sum(colSums(!is.na(sumData))>0)
   # st <- vector(mode = "list", length = nGoodCol)
   # i <- 1
-  # 
+  #
   # for(v in 1:length(stc)){
   #   if(any(!is.na(sumData[[stc[[v]]]]))){
   #     st[[i]]$values <-  sumData[[stc[[v]]]]
@@ -218,55 +221,56 @@ PALMOD2LiPD <- function(x, path = "dataLiPD"){   # x is list with data
   #     st[[i]]$variableName <- stcLiPD[[v]]
   #     st[[i]]$number <- i
   #     st[[i]]$units <- stu[v]
-  #     
-  #     
+  #
+  #
   #     i <- i+1
-  #     
+  #
   #   }
   # }
-  # 
+  #
   # names(st) <- sapply(st,"[[","variableName")
-  # 
+  #
   # sumTab <- vector(mode = "list",length = 1)
   # sumTab[[1]] <- st
-  
-  
+
+
   #chron method
   #get bacon parameters
   tgb <- which(sapply(e$BACON,length)==1)
-  
+
   method <- list(model = "Bacon", parameters = e$BACON[tgb])
-  
+
   #get age ensemble data
-  
+
   ae <- vector(mode = "list",length = 2)
-  
-  #assign depth 
+
+  #assign depth
   ae[[1]]$values <-  e$AgeEnsemblesBACON[,1]
   ae[[1]]$TSid <- createTSid()
   ae[[1]]$variableName <- "depth"
   ae[[1]]$number <- 1
   ae[[1]]$units <- "m"
-  
+
   #assign ageEnsemble
   ae[[2]]$values <-  e$AgeEnsemblesBACON[,-1]
   ae[[2]]$TSid <- createTSid()
   ae[[2]]$variableName <- "ageEnsemble"
   ae[[2]]$number <- seq(2, ncol(e$AgeEnsemblesBACON))
   ae[[2]]$units <- "ka"
-  
+
   names(ae) <- c("depth","ageEnsemble")
   ageEns <- vector(mode = "list", length = 1)
   ageEns[[1]] <- ae
 
-  
+
   #put the chron model together
   L$chronData[[1]]$model <- vector(mode = "list",length = 1)
   L$chronData[[1]]$model[[1]] <- list(method = method, ensembleTable = ageEns)
-  
+
   # sst ensembles ####
   # @Nick: please check, should this be included in paleodata?
-  L$sstEnsembles <- map(e$SurfaceTempEnsembles, function(x){
+  #yes
+  L$paleoData[[1]]$model  <- map(e$SurfaceTempEnsembles, function(x){
     list(
       method = list(
         model = ifelse(x$details$basis == "MgCa", "MgCaRB", "Bayspline"),
@@ -282,15 +286,17 @@ PALMOD2LiPD <- function(x, path = "dataLiPD"){   # x is list with data
         ),
         sstEnsemble = list(
           values =  x$ensembles[,-1],
-          TSid <- createTSid(),
-          variableName <- "sstEnsemble",
+          TSid = createTSid(),
+          variableName = "sstEnsemble",
           number = seq(2, ncol(x$ensembles)),
-          units = "ka"
+          units = "deg C"
         )
       )
     )
   })
-  
+
+  L$paleoData[[1]]$model[[1]]$ensembleTable[[1]]
+
   # sort out data links and publications ####
 
   # this simply takes all unique data links from the file
@@ -312,7 +318,7 @@ PALMOD2LiPD <- function(x, path = "dataLiPD"){   # x is list with data
   chronDOIs <- unique(e$chron$ChronSource[!is.na(e$chron$ChronSource)])
   chronDOIs <- unlist(sapply(chronDOIs, function(b) strsplit(b, '; ')))
 
-  allDOIs <- as.list(unique(c(pubDOIs, chronDOIs)))
+  allDOIs <- unique(c(pubDOIs, chronDOIs))
 
   pub <- vector(mode = "list",length = length(allDOIs))
   for(p in 1:length(pub)){
@@ -341,8 +347,8 @@ PALMOD2LiPD <- function(x, path = "dataLiPD"){   # x is list with data
 
   # for now, write to file
   #save(L, file = paste0(path, L$dataSetName, '.Rdata'))
-  
+
   # # write to LiPD
-  writeLipd(L, path = path)
+  writeLipd(L, path = file.path(path,paste0(L$dataSetName,".lpd")))
   #L2 <- readLipd("MD02_2529.lpd")
 }
